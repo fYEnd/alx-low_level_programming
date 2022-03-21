c29c88892127835ba0a663cbebf025c6e6b2bb28
Abdussalam Yahya <abdus.yahya@gmail.com>
2022-03-21 13:08:10 -0700

    Added header file for functions

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifendif MAIN_H
-#define MAIN_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +11,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+
